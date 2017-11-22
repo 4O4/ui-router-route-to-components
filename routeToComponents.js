e3b96a229b68dcb45896ec84d75c1f8c820c8eb6
4O4 <pk.pawelo@gmail.com>
2017-11-22 09:16:51 +0100

    Fix named bindings handling and add support for optional bindings

--- routeToComponents.js
+++ routeToComponents.js
@@ -22,32 +22,33 @@
 		return controller;
 	}
 
-	var getCompInputs = function getCompInputs($injector, componentName) {
-		return $injector.get(componentName + "Directive").map(function (directive) {
-			return Object.keys(directive.bindToController);
-		}).reduce(unnestR, []);
-	};
-
-	var getAttrs = function getAttrs($injector, componentName) {
+	var getCompInputs = function getCompInputs($injector, componentName, resolve) {
 		return $injector.get(componentName + "Directive").map(function (directive) {
 			var inputs = [];
 			for (var key in directive.bindToController) {
 				if (directive.bindToController.hasOwnProperty(key)) {
 					var binding = directive.bindToController[key];
+					
 					if (!binding || !binding.length) {
 						continue;
 					}
 
 					if (binding.length === 1) {
-						inputs.push({
-							key: key,
-							value: key
-						});
+						inputs.push(key);
+						continue;
+					} 
+					
+					if (binding.substring(1, 2) === '?') {
+						var resolveName = key;
+						if (binding.length > 2) {
+							resolveName = binding.substring(2);
+						}
+						
+						if (resolve.hasOwnProperty(resolveName)) {
+							inputs.push(resolveName);
+						}
 					} else {
-						inputs.push({
-							key: binding.substring(1),
-							value: key
-						});
+						inputs.push(binding.substring(1));
 					}
 				}
 			}
@@ -59,17 +60,15 @@
 		return acc.concat(array);
 	};
 
-	function setStateDef(stateDef) {
+	function setStateDef(stateDef, resolve) {
 		stateDef.controllerProvider = function ($injector) {
-			return bindResolves.apply(null, getCompInputs($injector, stateDef.component));
+			return bindResolves.apply(null, getCompInputs($injector, stateDef.component, resolve));
 		};
 		stateDef.controllerProvider.$inject = ['$injector'];
 
 		stateDef.templateProvider = function ($injector) {
-			var attrs = getAttrs($injector, stateDef.component).map(function (object) {
-				var key = object.key;
-				var value = object.value;
-				return kebobString(key) + '="' + value + '"';
+			var attrs = getCompInputs($injector, stateDef.component, resolve).map(function (key) {
+				return kebobString(key) + '="' + key + '"';
 			}).join(' ');
 
 			var kebobName = kebobString(stateDef.component);
@@ -94,7 +93,7 @@
 			if (stateDef.views) {
 				for (var view in stateDef.views) {
 					if (stateDef.views[view].component) {
-						setStateDef(stateDef.views[view]);
+						setStateDef(stateDef.views[view], stateDef.resolve);
 					}
 				}
 			}
